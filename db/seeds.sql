1c79deb7e1f8b36fb10c697741a428f94de68640
Jacob Petersen <jcb.petersen1995@gmail.com>
2020-03-16 21:17:46 -0600

    first push. layout of app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+--Inserting Into Tables
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) values ("Cheeseburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) values ("BBQ Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) values ("Turkey Burger", FALSE);
